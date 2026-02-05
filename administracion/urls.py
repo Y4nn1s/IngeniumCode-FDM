b6f42d935a4f4ca05fecb24fb025898dcab3cd81
Alkeides27 <alkeidesantonio@gmail.com>
2026-02-04 21:38:29 -0400

    CRUD para Cuerpo Técnico

--- administracion/urls.py
+++ administracion/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('entrenadores/', views.entrenador_list, name='entrenador_list'),
+    path('entrenadores/nuevo/', views.EntrenadorCreateView.as_view(), name='entrenador_create'),
+    path('entrenadores/<int:pk>/editar/', views.EntrenadorUpdateView.as_view(), name='entrenador_update'),
+    path('entrenadores/<int:pk>/eliminar/', views.EntrenadorDeleteView.as_view(), name='entrenador_delete'),
 ]
+
--- administracion/views.py
+++ administracion/views.py
@@ -1,6 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 
+from .forms import EntrenadorForm
 from .models import Entrenador
 
 
@@ -17,3 +22,46 @@ def entrenador_list(request):
     return render(request, 'administracion/entrenador_list.html', {
         'entrenadores': entrenadores,
     })
+
+
+class SuperusuarioRequiredMixin(UserPassesTestMixin):
+    """Mixin que restringe el acceso solo a superusuarios."""
+    
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class EntrenadorCreateView(LoginRequiredMixin, SuperusuarioRequiredMixin, CreateView):
+    """Vista para crear un nuevo entrenador (solo superusuarios)."""
+    model = Entrenador
+    form_class = EntrenadorForm
+    template_name = 'administracion/entrenador_form.html'
+    success_url = reverse_lazy('entrenador_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Entrenador creado exitosamente.')
+        return super().form_valid(form)
+
+
+class EntrenadorUpdateView(LoginRequiredMixin, SuperusuarioRequiredMixin, UpdateView):
+    """Vista para editar un entrenador existente (solo superusuarios)."""
+    model = Entrenador
+    form_class = EntrenadorForm
+    template_name = 'administracion/entrenador_form.html'
+    success_url = reverse_lazy('entrenador_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Entrenador actualizado exitosamente.')
+        return super().form_valid(form)
+
+
+class EntrenadorDeleteView(LoginRequiredMixin, SuperusuarioRequiredMixin, DeleteView):
+    """Vista para eliminar un entrenador (solo superusuarios)."""
+    model = Entrenador
+    template_name = 'administracion/entrenador_confirm_delete.html'
+    success_url = reverse_lazy('entrenador_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Entrenador eliminado exitosamente.')
+        return super().delete(request, *args, **kwargs)
+
