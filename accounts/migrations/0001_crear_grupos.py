79f4bd5127f49987cd8312a2db198e9214fd8b84
Alkeides27 <alkeidesantonio@gmail.com>
2026-05-01 04:06:24 -0400

    feat(accounts): migración de grupos de roles del sistema

---
+++ accounts/migrations/0001_crear_grupos.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+GRUPOS = [
+    'Tesoreria',
+    'CoordinadorGeneral',
+    'CoordinadorDeportivo',
+    'Entrenador',
+]
+
+
+def crear_grupos(apps, schema_editor):
+    Group = apps.get_model('auth', 'Group')
+    for nombre in GRUPOS:
+        Group.objects.get_or_create(name=nombre)
+
+
+def eliminar_grupos(apps, schema_editor):
+    Group = apps.get_model('auth', 'Group')
+    Group.objects.filter(name__in=GRUPOS).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunPython(crear_grupos, eliminar_grupos),
+    ]
